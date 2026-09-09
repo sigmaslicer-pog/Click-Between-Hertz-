2272dbec31f1a8e9f91a1e761e3b5951b61638f7
sigmaslicer-pog <Rizzlordsigma69@outlook.com>
2026-09-09 16:19:18 -0400

    Update project files and fix build headers

--- src/main.cpp
+++ src/main.cpp
@@ -106,6 +106,7 @@ class $modify(MyMenuLayer, MenuLayer) {
 #include <cmath>
 #include <cstddef>
 
+#include "include/AllHeaders.hpp"
 #include "CBHQueue.hpp"
 #include "SIPlayerObject.hpp"
 #include "SubtickInputs.hpp"
