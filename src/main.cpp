da106fe7a2e4b96bc4237781e6ac5adb73c9f988
sigmaslicer-pog <Rizzlordsigma69@outlook.com>
2026-09-09 16:07:48 -0400

    Fix PlayerButton type cast and include header in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -106,7 +106,7 @@ class $modify(MyMenuLayer, MenuLayer) {
 #include <cmath>
 #include <cstddef>
 
-#include "InputQueue.hpp"
+#include "CBHQueue.hpp"
 #include "SIPlayerObject.hpp"
 #include "SubtickInputs.hpp"
 
